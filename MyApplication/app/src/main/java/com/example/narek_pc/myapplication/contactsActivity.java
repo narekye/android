3823faae8827f5bf44bb45108b7ced26e53cbfa6
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-08-02 16:13:16 +0400

    exception handling

--- MyApplication/app/src/main/java/com/example/narek_pc/myapplication/contactsActivity.java
+++ MyApplication/app/src/main/java/com/example/narek_pc/myapplication/contactsActivity.java
@@ -1,13 +1,12 @@
 package com.example.narek_pc.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.http.AndroidHttpClient;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -22,18 +21,30 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-public class MainActivity extends AppCompatActivity {
+public class contactsActivity extends AppCompatActivity {
     private static String url = "http://crmbetd.azurewebsites.net/api/token";
     Context mcontext;
     public static String responseString;
     HttpResponse response = null;
     StatusLine statusLine = null;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contacts);
+        String token = MainActivity.responseString;
+        // TextView text = (TextView) findViewById(R.id.title);
+        // text.setText(token);
+    }
+
+    private String userAgent() {
+        String userAgent = null;
+        if (userAgent != null) {
+        }
+        if (userAgent == null) {
+            userAgent = "Mozilla/5.0 (X11; U; Linux i686; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13";
+        }
+        return userAgent;
     }
 
     public void Login(View view) throws InterruptedException {
@@ -55,51 +66,19 @@ public class MainActivity extends AppCompatActivity {
                     HttpEntity en = response.getEntity();
                     String resp = EntityUtils.toString(en, "UTF-8");
                     responseString = resp.toString();
-                    if (statusLine.getStatusCode() == 400) {
-                        showMessage("The username/password is incorrect.");
-                    } else {
-                        showMessage("Please wait.");
-                        JSONObject json = new JSONObject(responseString.toString());
-                        responseString = json.getString("access_token");
-                        Thread.sleep(3000);
-                        next();
-                    }
+                    JSONObject json = new JSONObject(responseString.toString());
+                    responseString = json.getString("access_token");
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
-    }
+        // next();
+        Thread.sleep(3000);
 
-    private void showMessage(final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MainActivity.this,
-                        message,
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private String userAgent() {
-        return "Mozilla/5.0 (X11; U; Linux i686; zh-CN; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13";
-    }
-
-    private void next() {
-        Intent intent = new Intent(getApplicationContext(), contactsActivity.class);
-        startActivity(intent);
-    }
-
-    public void eraseonclick(View view) {
-        EditText filed = (EditText) findViewById(view.getId());
-        filed.setText("");
     }
 }
